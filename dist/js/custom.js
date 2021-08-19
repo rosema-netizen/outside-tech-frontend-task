a2ac0d21ab26b6a60b49f7665bf18ac939a64acf
ROSEMA <rosemashak789@gmail.com>
2021-08-19 06:14:29 +0545

    Initiate slick in card-bgnone for sp
    slick slider added to card without bg for sp screen in learn block

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -4,7 +4,52 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 3,
         dots:false,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 420,
+              settings: {
+                slidesToShow: 1.5,
+                slidesToScroll: 1.5
+              }
+            },
+          ]
+      });
+
+
+      $('.bgnone-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots:false,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 420,
+              settings: {
+                slidesToShow: 1.5,
+                slidesToScroll: 1.5
+              }
+            },
+          ]
       });
           
   });
